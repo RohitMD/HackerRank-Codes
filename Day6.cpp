dea360c022d9ce3dd5468c41a7a3ea2ff3b12fa3
RohitMD <84699703+RohitMD@users.noreply.github.com>
2021-05-25 19:53:29 +0530

    Update Day6.cpp
    This program is for printing Word's even indices and then odd indices. 
    Input :- No. of testcases T , T times enter the string.
    Output:- even indices <space> odd indices each on a new line for each string input.

--- Day6.cpp
+++ Day6.cpp
@@ -21,21 +21,21 @@ int main() {
     for (int i=0;i<T;i++)
     {
         cin>>Word[i];
-        cout<<Word[i].size()<<endl; /* don't use sizeof() here
-        When sizeof() is used with the data types such as int, float, char… etc it simply returns the amount of memory is allocated to that data types
-*/
+
     }
     for (int i=0;i<T;i++)
     {
-        for(int j =0; j<Word[i].size() ; j++ ) //j=even means odd numbered word and vice verca
+        for(int j =0; j<Word[i].size() ; j++ )/* don't use sizeof() here
+        When sizeof() is used with the data types such as int, float, char… etc it simply returns the amount of memory is allocated to that data types
+*/
         {
-            if(! EvenOdd(j) ) cout<<Word[i][j];  //when j will be odd, even numbered letter will be printed
+            if(EvenOdd(j) ) cout<<Word[i][j];  //when j will be odd, odd indexed character will be printed
         }
         cout <<" "; //a space between even and odd printings
 
-        for(int j =0; j<Word[i].size() ; j++ ) //j=even means odd numbered word and vice verca
+        for(int j =0; j<Word[i].size() ; j++ )
         {
-            if( EvenOdd(j) ) cout<<Word[i][j];  //when j will be even, odd numbered letter will be printed
+            if( !EvenOdd(j) ) cout<<Word[i][j];  //when j will be even, even indexed character will be printed
         }
         cout<<endl;
 
